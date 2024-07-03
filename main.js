209486adb0fdd072395b3a80140b4adf940334ae
ruilong.zhao <ruilong.zhao@dmall.com>
2024-07-03 16:44:51 +0800

    init

--- main.js
+++ main.js
@@ -12,4 +12,15 @@ const createWindow=()=>{
 
 app.on('ready',()=>{
     createWindow();
+    app.on('activate',()=>{
+        if(BrowserWindow.getAllWindows().length===0){
+            createWindow();
+        }
+    })
+})
+
+app.on('window-all-closed',()=>{
+    if(process.platform!=='darwin'){
+        app.quit();
+    }
 })
