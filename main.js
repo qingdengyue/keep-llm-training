0aa765738155948d9bc2980406c3f5fda38c9081
ruilong.zhao <ruilong.zhao@dmall.com>
2024-07-04 13:36:50 +0800

    init

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const {app,BrowserWindow} = require('electron')
+const path = require('node:path')
 const createWindow=()=>{
     const win=new BrowserWindow({
         width:800,
         height:600,
         webPreferences:{
-            nodeIntegration:true
+            preload:path.join(__dirname,'preload.js')
         }
     })
     win.loadFile('index.html')
